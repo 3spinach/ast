c3aaffe93e1518695c566b82dfe675fd3612cfc1
3spinach <242190586+3spinach@users.noreply.github.com>
2025-11-06 14:00:29 +0900

    Update run_esc.sh

--- egs/esc50/run_esc.sh
+++ egs/esc50/run_esc.sh
@@ -10,8 +10,8 @@
 
 set -x
 # comment this line if not running on sls cluster
-. /data/sls/scratch/share-201907/slstoolchainrc
-source ../../venvast/bin/activate
+# . /data/sls/scratch/share-201907/slstoolchainrc
+# source ../../venvast/bin/activate
 export TORCH_HOME=../../pretrained_models
 
 model=ast
@@ -29,7 +29,7 @@ freqm=24
 timem=96
 mixup=0
 epoch=25
-batch_size=48
+batch_size=32
 fstride=10
 tstride=10
 
